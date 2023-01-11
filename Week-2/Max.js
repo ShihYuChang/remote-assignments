e2a0c6b09a1959b3a6504f7b5dbc320220bc0177
Sam Chang <39560452+ShihYuChang@users.noreply.github.com>
2023-01-11 04:44:18 +0000

    Remove old files

--- Week-2/Max.js
+++
@@ -1,7 +0,0 @@
-function max(numbers) {
-    numbers.sort();
-    return numbers[numbers.length -1];
-    }
-
-    console.log(max([1, 2, 4, 5])); // expected output: 5
-    console.log(max([5, 2, 7, 1, 6])); // expected output: 7
