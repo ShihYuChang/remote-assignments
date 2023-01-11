bb916b87d31db4b4bc97952d225bcf8083e3b849
Sam Chang <39560452+ShihYuChang@users.noreply.github.com>
2023-01-11 11:30:19 +0000

    Add file Assignment-2.js

---
+++ Week-2/Assignment-2.js
@@ -0,0 +1,18 @@
+function calculate(numList) {
+    if ( numList.op === '+') {
+        result = numList.n1 + numList.n2;
+        return result;
+    }
+
+    else if ( numList.op === '-') {
+        result = numList.n1 - numList.n2;
+        return result;
+    }
+
+    else {
+        let errorMsg = 'Not supported';
+        return errorMsg;
+
+    }
+
+}
