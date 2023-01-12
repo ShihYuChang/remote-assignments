0aaca7cd1922ecad959b06e394656d2d40139175
Sam Chang <39560452+ShihYuChang@users.noreply.github.com>
2023-01-12 10:51:50 +0000

    Modified Assignment-3.js

--- Week-2/Assignment-3.js
+++ Week-2/Assignment-3.js
@@ -1,21 +1,17 @@
-// Create a object to store all the projects and their discounted price.
-var finalProductList = {};
 
-// Create a function to generate the discounted price
+// Create a varible for the total discounted price for all products and set it to zero initailly.
+var totalPrice = 0;
+
+// Create the function 'calculate' to generate the totalPrice
 function calculate(data) {
     for ( let i = 0; i < data.products.length; i++ ) {
         var productPrice = data.products[i].price;
         var productDiscount = data.discount;
-        
         var finalPrice = productPrice * productDiscount;
-        //store the products and thier discounted price to the fianlProductList object.
-        finalProductList += `
-        ${productList.products[i].name}: ${finalPrice}
-        `; 
+        totalPrice += finalPrice;                
     }
-    return finalProductList;
+    return totalPrice;
 }
-
 
 
 // Create a variable to store the object of product names and prices
@@ -37,5 +33,4 @@ const productList = {
     ]
 }
 
-// Create a string to show the products and their discounted price.
 console.log(calculate(productList));
