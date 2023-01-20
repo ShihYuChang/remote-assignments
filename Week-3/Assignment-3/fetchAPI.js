dc8f60d264c65bedf1e32d840f70eb3df17b2b3b
Sam Chang <sam21323@gmail.com>
2023-01-20 14:33:50 +0800

    codes concised

--- Week-3/Assignment-3/fetchAPI.js
+++ Week-3/Assignment-3/fetchAPI.js
@@ -2,9 +2,7 @@ function fetchData(url) {
   const main = document.querySelector(".mainBody");
   const inputValue = document.getElementById("inputNum").value;
   fetch(`http://localhost:3000/getData?number=${inputValue}`)
-    .then((response) => {
-      return response.text();
-    })
+    .then((response) => response.text())
     .then((response) => {
       main.innerHTML = response;
     })
