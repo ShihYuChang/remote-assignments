488baff458bcc7bef333190a3a1cc238d0df02dd
Sam Chang <sam21323@gmail.com>
2023-01-24 00:45:13 +0800

    Upload Assignment-2 folder

---
+++ Week-4/Assignment-2/script.js
@@ -0,0 +1,26 @@
+function ajax(url) {
+  return fetch(url)
+    .then((res) => res.json()) //Parse the data to json format
+    .catch((error) => console.log(`Error: ${error}`));
+}
+
+function render(data) {
+  data.map((product) =>
+    productDivSelector.insertAdjacentHTML(
+      "beforeend",
+      `
+            <h2>${product.name}</h2>
+            <h3>$${product.price}</h3>
+            <p>${product.description}</p>
+            <br></br>
+  `
+    )
+  );
+}
+
+const url = "https://appworks-school.github.io/Remote-Assignment-Data/products";
+const productDivSelector = document.querySelector(".product");
+
+ajax(url).then((data) => {
+  render(data);
+});
