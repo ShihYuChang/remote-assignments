36d497bb870bd184dcff2ffc1f33ba052f0e6b91
Sam Chang <sam21323@gmail.com>
2023-01-30 17:41:15 +0800

    Rename componants

--- Week-4/Assignment-3/By-Webpack/src/index.js
+++ Week-4/Assignment-3/By-Webpack/src/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./Components/header";
-import MainContent from "./Components/main-content";
-import Banner from "./Components/banner";
-import Footer from "./Components/footer";
+import Header from "./Components/Header";
+import MainContent from "./Components/MainContent";
+import Banner from "./Components/Banner";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
