81f2145586c8616bbbc0516dc7d5f77a2663ccec
Sam Chang <sam21323@gmail.com>
2023-02-01 00:04:45 +0800

    Add By-Webpack folder and files

---
+++ Week-4/Assignment-4/By-Webpack/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ContentTable from "./Components/ContentTable";
+import "./style.css";
+import "./normalize.css";
+
+// Render
+const App = () => (
+  <div className="wrapper">
+    <div className="mainContent">
+      <ContentTable />
+    </div>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ Week-4/Assignment-4/By-Webpack/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/",
+    libraryTarget: "var",
+    library: "bundle",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    headers: {
+      "Content-Type": "application/javascript",
+    },
+  },
+  mode: "development",
+};
